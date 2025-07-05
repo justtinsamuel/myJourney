8e67131dee5d04b4e97b3fa449e4c9f6d79f1b41
justtinsamuel <justtinsamuel01@gmail.com>
2025-07-05 14:16:45 +0700

    latihan algoritma dan psuedocode

---
+++ exercise/index.js
@@ -0,0 +1,51 @@
+// ALGORITMA PSUEDOCODE
+
+/*
+# Study Case 1
+Hitung luas persegi panjang:
+
+Jawab:
+Algoritma:
+tentukan variable panjang
+tentukan variable lebar
+tentukan variable luas persegi panjang dengan panjang x tinggi
+tampilkan hasil luas
+
+Psuedocode:
+STORE panjang WITH 4;
+STORE lebar WITH 2;
+STORE luas persegi panjang WITH panjang MUL lebar;
+PPRINT luas persegi panjang;
+
+Code:
+let panjang = 4;
+let lebar = 2;
+let luasPersegiPanjang = panjang * lebar;
+console.log(luasPersegiPanjang);
+*/
+
+/*
+# Study Case 2
+Hitung luas lingkaran: phi * r * r;
+
+Algoritma:
+tentukan variable phi;
+tentukan variable r;
+tentukan luasLingkaran;
+buat luarLingkaran = phi * r * r;
+tampilkan luasLingkaran
+
+Psuedocode:
+STORE phi WITH 3,14;
+STORE r with 7;
+STORE luasLingkaran = phi MUL r MUL r;
+PRINT luasLingkaran;
+
+Code:
+const phi = 3.14;
+let r = 7;
+let luasLingkaran = phi * r * r;
+console.log(luasLingkaran);
+*/
+
+console.log("Hello World");
