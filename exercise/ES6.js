a73ea3d407a54d1b45b26bdee5ea4a7272180e97
justtinsamuel <justtinsamuel01@gmail.com>
2025-07-13 16:05:29 +0700

    New Mater

--- exercise/ES6.js
+++ exercise/ES6.js
@@ -139,22 +139,94 @@
 
 // PUSH, nambah elemen ke ARRAY
 
-const product = [
-  { name: "dennim", price: 150000 },
-  { name: "jacket", price: 210000 },
-  { name: "shoes", price: 220000 },
+// const product = [
+//   { name: "dennim", price: 150000 },
+//   { name: "jacket", price: 210000 },
+//   { name: "shoes", price: 220000 },
+// ];
+
+// console.log(product);
+
+// // PUSH
+// product.push({
+//     name: 'tas', price: 115000,
+// })
+
+// console.log(product);
+
+// // POP
+// product.pop();
+// console.log(product);
+
+/* CRUD (Create, Read, Update, Delete) */
+
+let students = [
+  { nim: 1, name: "Prabo", major: "Computer Science", isGraduated: true },
+  { nim: 2, name: "Joko", major: "Computer Science", isGraduated: false },
+  { nim: 3, name: "Mega", major: "Accounting", isGraduated: true },
 ];
 
-console.log(product);
+// READ
+showStudents = () => {
+  console.log(`Student list:`);
+  students.forEach((stud, idx) => {
+    stud.isGraduated
+      ? console.log(`${idx + 1}. [v] ${stud.name}, ${stud.major}`)
+      : console.log(`${idx + 1}. [ ] ${stud.name}, ${stud.major}`);
+  });
+};
 
-// PUSH
-product.push({
-    name: 'tas', price: 115000,
-})
+// CREATE
+addStudents = (name, major) => {
+  let lastIndex = students.length - 1;
+  let nim = students[lastIndex].nim++;
+  let isGraduated = false;
 
-console.log(product);
+  students.push({
+    nim,
+    name,
+    major,
+    isGraduated,
+  });
+};
 
-// POP
-product.pop();
-console.log(product);
+showStudents();
+console.log("\n");
+addStudents("Jojon", "Accounting");
+showStudents();
+console.log("\n");
 
+// UPDATE
+function updateStudents(nim, newName) {
+  let isUpdated = false;
+
+  students.forEach((stud) => {
+    if (stud.nim === nim) {
+      stud.name = newName;
+      isUpdated = true;
+    }
+  });
+
+  if (!isUpdated) {
+    console.log(`NIM ${nim} tidak ditemukan!`);
+  }
+}
+
+updateStudents(1, "Prabowo");
+showStudents();
+console.log("\n");
+
+// DELETE
+function deleteStudents(nim) {
+  let temp = [];
+  for (let i = 0; i < students.length; i++) {
+    if (students[i].nim !== nim) {
+      temp.push(students[i]);
+    } else {
+    }
+  }
+  students = temp;
+}
+
+deleteStudents(2);
+showStudents();
